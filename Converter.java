19531c9e448c9f4a9d01b2bd12080b8c1306964c
basecamp <jmorgan@basecampcodingacademy.org>
2019-01-03 10:18:11 -0600

    We did it

--- Converter.java
+++ Converter.java
@@ -8,20 +8,59 @@ public class Converter {
     }
 
     public Double toFahrenheit() {
-        if (this.unit.equals("F")) {
-            return this.magnitude;
-        } else if (this.unit.equals("C")) {
-            return this.magnitude * 9 / 5 + 32;
+        if (unit.equals("F")) {
+            return magnitude;
+        } else if (unit.equals("C")) {
+            return magnitude * 9 / 5 + 32;
+        } else if (unit.equals("K")) {
+            return magnitude - 32 * 5 / 9 + 273.15;
+        } else if (unit.equals("R")) {
+            return magnitude - 459.67;
+
         } else {
             return null;
         }
     }
 
     public Double toCelsius() {
-        if (this.unit.equals("F")) {
-            return (this.magnitude - 32) * 5 / 9;
-        } else if (this.unit.equals("C")) {
-            return this.magnitude;
+        if (unit.equals("F")) {
+            return (magnitude - 32) * 5 / 9;
+        } else if (unit.equals("C")) {
+            return magnitude;
+        } else if (unit.equals("K")) {
+            return (magnitude + 273.15);
+        } else if (unit.equals("R")) {
+            return (magnitude - 491.67) * 5 / 9;
+        }
+
+        else {
+            return null;
+        }
+    }
+
+    public Double toKelvin() {
+        if (unit.equals("F")) {
+            return (magnitude + 459.67) * 5 / 9;
+        } else if (unit.equals("C")) {
+            return magnitude + 273.15;
+        } else if (unit.equals("K")) {
+            return magnitude;
+        } else if (unit.equals("R")) {
+            return magnitude * 5 / 9;
+        } else {
+            return null;
+        }
+    }
+
+    public Double toRankine() {
+        if (unit.equals("F")) {
+            return (magnitude + 459.67);
+        } else if (unit.equals("C")) {
+            return (magnitude + 273.15) * 9 / 5;
+        } else if (unit.equals("K")) {
+            return magnitude * 9 / 5;
+        } else if (unit.equals("R")) {
+            return magnitude;
         } else {
             return null;
         }
